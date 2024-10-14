fbde55ccb4b3f0b88e34b680f6dab447aeccb38c
lilin <lilin@bailongma-inc.com>
2024-10-14 17:58:13 +0800

    feat: fix

--- chromePlugin/src/App.jsx
+++ chromePlugin/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   // 简历信息
   const [info, setInfo] = useState(null);
   // 简历编号
-  const [jianCode, setJianCode] = useState("");
+  const [cvId, setCvId] = useState("");
   // 当前用户名
   const [name, setName] = useState("");
   const [token, setToken] = useState("");
@@ -23,14 +23,14 @@ function App() {
     setOpen(false);
   };
   const getQuery = (url) => {
-    const result = {}
-    const reg1 = /([^?=&#]+)=([^?=&#]+)/g
-    const reg2 = /#([^?=&#]+)/g
-    url.replace(reg1, (n, x, y) => (result[x] = y))
-    url.replace(reg2, (n, x) => (result['HASH'] = x))
-    console.log('result:', result);
-    return result
-  }
+    const result = {};
+    const reg1 = /([^?=&#]+)=([^?=&#]+)/g;
+    const reg2 = /#([^?=&#]+)/g;
+    url.replace(reg1, (n, x, y) => (result[x] = y));
+    url.replace(reg2, (n, x) => (result["HASH"] = x));
+    console.log("result:", result);
+    return result;
+  };
 
   useEffect(() => {
     // 监听插件回调
@@ -38,7 +38,7 @@ function App() {
       // 获得简历编号、用户名、行为
       const { url, name, action, token } = res?.data || {};
       const { res_id_encode } = getQuery(url) || {};
-      console.log('前台111', token);
+      console.log("前台111", token);
       if (!token) {
         messageApi.open({
           type: "error",
@@ -47,7 +47,7 @@ function App() {
         return;
       }
       console.log("lilin222", res_id_encode, name, action, token);
-      setJianCode(res_id_encode);
+      setCvId(res_id_encode);
       setName(name);
       setToken(token);
       // 进入猎聘详情页，获取存储的简历信息
@@ -58,20 +58,11 @@ function App() {
           })
           .catch((err) => {
             console.log("Error:", err);
-            messageApi.open({
-              type: "error",
-              content: "请先登录内部系统",
-            });
           });
       }
       // 点击联系按钮时，触发修改简历阅读状态
       if (action === "click") {
-        setCVStatus({ cv_id: res_id_encode, token }).catch(() => {
-          messageApi.open({
-            type: "error",
-            content: "请先登录内部系统",
-          });
-        });
+        setCVStatus({ cv_id: res_id_encode, token }).catch(() => {});
       }
     });
   }, []);
@@ -89,7 +80,7 @@ function App() {
         mask={false}
         closeIcon={<DoubleRightOutlined />}
       >
-        <Home info={info} jianCode={jianCode} name={name} token={token} />
+        <Home info={info} cvId={cvId} name={name} token={token} />
       </Drawer>
     </>
   );
--- chromePlugin/src/pages/Home/index.jsx
+++ chromePlugin/src/pages/Home/index.jsx
@@ -9,24 +9,24 @@ const Home = (props) => {
   const [messageApi, contextHolder] = message.useMessage();
   // const userInfo = useSelector((state) => state.userInfo);
   // const { name } = userInfo || {};
-  const { jianCode, info, name, token } = props || {};
+  const { cvId, info, name, token } = props || {};
 
   const onFinish = (values) => {
-    console.log("Success:", { ...values, name, jianCode });
+    console.log("Success:", { ...values, name, cv_id: cvId });
     // 备注、用户名、简历编号
-    addRemark({ ...values, name, jianCode, token })
+    addRemark({ ...values, created_by: name, cv_id: cvId, token })
       .then((res) => {
         const { code, data } = res || {};
-        code === 1 && messageApi.open({
-          type: "success",
-          content: "提交成功",
-        });
+        code === 1 &&
+          messageApi.open({
+            type: "success",
+            content: "提交成功",
+          });
       })
       .catch((err) => {
         console.log("Error:", err);
       });
   };
-
 
   return (
     <div className={styles.home}>
@@ -35,56 +35,57 @@ const Home = (props) => {
         <UserOutlined />
         <span>
           该候选人
-          {info ? "已" : "未"}联系
+          {info.length ? "已" : "未"}联系
         </span>
       </p>
-      {(
+      {info.length && (
         <div>
-          <Form
-            name="basic"
-            layout="vertical"
-            labelCol={{
-              span: 8,
-            }}
-            wrapperCol={{
-              span: 16,
-            }}
-            style={{
-              maxWidth: 600,
-            }}
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-            autoComplete="off"
-          >
-            <Form.Item label="备注" name="describe">
-              <TextArea rows={4} />
-            </Form.Item>
+          <p>
+            <span>备注人：{ info[0] && info[0].created_by }</span>
+          </p>
+          <p>
+            <span>备注内容：{ info[0] && info[0].content }</span>
+          </p>
+          <p>
+            <span>备注时间：{ info[0] && info[0].created_at }</span>
+          </p>
+        </div>
+      )}
+      <div>
+        <Form
+          name="basic"
+          layout="vertical"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item label="备注" name="content">
+            <TextArea rows={4} />
+          </Form.Item>
 
-            <Form.Item
-              wrapperCol={{
-                offset: 0,
-                span: 1,
-              }}
-            >
-              <Button type="primary" htmlType="submit">
-                提交
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      )}
-      {info && (
-        <div>
-          <p>
-            <span>联系人：13569127156</span>
-          </p>
-          <p>
-            <span>备注：</span>
-          </p>
-        </div>
-      )}
+          <Form.Item
+            wrapperCol={{
+              offset: 0,
+              span: 1,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              提交
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };

0ca36feb8278ffc31a5ad8bebb6977e1793bcf9b
lilin <lilin@bailongma-inc.com>
2024-10-14 16:28:24 +0800

    feat: proxy

--- chromePlugin/vite.config.js
+++ chromePlugin/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/chrome-plugin": {
+        target: "http://localhost:8888",
+        changeOrigin: true,
+      },
+    },
+  },
 })
